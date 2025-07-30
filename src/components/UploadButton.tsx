881d4e4eda915aa2e062e721e19d419f53dea6e3
Eamon <eamondevine@gmail.com>
2025-07-30 12:10:09 +0800

    Set unsigned to signed with cldupwidg, ya know

--- src/components/UploadButton.tsx
+++ src/components/UploadButton.tsx
@@ -1,9 +1,20 @@
 "use client";
 import React from "react";
-import { CldUploadButton } from "next-cloudinary";
+import { CldUploadWidget } from "next-cloudinary";
+import { Upload } from "lucide-react";
 
 const UploadButton = () => {
-  return <CldUploadButton uploadPreset="ml_default" />;
+  return (
+    <CldUploadWidget signatureEndpoint="/api/sign-cloudinary-params">
+      {({ open }) => {
+        return (
+          <button onClick={() => open()}>
+            <Upload className="h-10 w-10 mr-10" />
+          </button>
+        );
+      }}
+    </CldUploadWidget>
+  );
 };
 
 export default UploadButton;
