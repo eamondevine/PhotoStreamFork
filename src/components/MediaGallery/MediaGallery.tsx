23499d05702bff55a00a918f8f04d7f3e3576195
Eamon <eamondevine@gmail.com>
2025-08-17 12:05:35 +0800

    Updated code fixed bug in MediaGallery didn't need the check for env variables there, only in page.tsx - parent of MediaGallery. For showing AWS S3 images on frontend: I can do that without any special JSON permissions or Cors, BUT if I want to allow downloading or PUT or other requests, gotta change those settings in console.

--- src/components/MediaGallery/MediaGallery.tsx
+++ src/components/MediaGallery/MediaGallery.tsx
@@ -67,18 +67,6 @@ const MediaGallery = ({
     }
   }
 
-  if (!process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY) {
-    throw new Error("Missing AWS credentials in environment variables");
-  }
-
-  const client = new S3Client({
-    region: "ap-southeast-1",
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    },
-  });
-
   return (
     <>
       <div className="text-[3rem] flex flex-col items-center">
